f6338a0d31d903256679df0595c61c9e2ac15600
Teodoro <teo.calle.lara@gmail.com>
2022-08-15 01:16:16 -0500

    Se agrega placeholder, se implementa material design

--- app/src/main/java/calle/lara/teo/dccomics/RegistroSuperHeroeActivity.kt
+++ app/src/main/java/calle/lara/teo/dccomics/RegistroSuperHeroeActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import com.google.android.material.textfield.TextInputEditText
 
  class RegistroSuperHeroeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,8 +14,9 @@ import android.widget.TextView
 
         //Creo una instancia de la clase button y la asocio al boton que se creó en la interfaz
         val btnRegister : Button = findViewById(R.id.btn_register)
-        val etNombre : EditText = findViewById(R.id.etHeroName)
+        val etNombre: EditText = findViewById(R.id.etHeroName)
         val tvInfo : TextView = findViewById(R.id.tv_info)
+        val tietEstatura : TextInputEditText = findViewById(R.id.tiet_estatura)
 
         /*
         *Quiero que lo que escriba en el EditText se vea en el TextView cuando haga clic sobre el
@@ -24,7 +26,9 @@ import android.widget.TextView
            // val nombre = etNombre.text //Esto es lo mismo que en java nombre = etNombre.getText()
            // tvInfo.text = nombre //Esto es lo mismo que en java tvInfo.setText(nombre)
 
-            tvInfo.text = etNombre.text // Esto hace lo mismo que las 2 instrucciones anteriores
+            tvInfo.text = getString(R.string.info,etNombre.text.toString(),tietEstatura.text.toString().toFloat()) // Esto hace lo mismo que las 2 instrucciones anteriores
+
+            //val string:Float = tietEstatura.text.toString().toFloat()
 
             //Si quisiera almacenar la info en una base de datos solo sería llamar el método y ponerle
             //la data que quiero enviar cuando haga click
