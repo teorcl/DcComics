6ae8b1b745932cae3b7366f18d177b1c4ec5cac6
Teodoro <teo.calle.lara@gmail.com>
2022-08-15 11:49:40 -0500

    Se agrega radioButton y CheckBox

--- app/src/main/java/calle/lara/teo/dccomics/RegistroSuperHeroeActivity.kt
+++ app/src/main/java/calle/lara/teo/dccomics/RegistroSuperHeroeActivity.kt
@@ -1,10 +1,8 @@
  package calle.lara.teo.dccomics
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.textfield.TextInputEditText
 
  class RegistroSuperHeroeActivity : AppCompatActivity() {
@@ -17,6 +15,12 @@ import com.google.android.material.textfield.TextInputEditText
         val etNombre: EditText = findViewById(R.id.etHeroName)
         val tvInfo : TextView = findViewById(R.id.tv_info)
         val tietEstatura : TextInputEditText = findViewById(R.id.tiet_estatura)
+        val rbMasculino : RadioButton = findViewById(R.id.rb_masculino)
+       // val rbFemenino : RadioButton = findViewById(R.id.rb_femenino)
+        val cboxFuerza : CheckBox = findViewById(R.id.cbox_super_fuerza)
+        val cboxSpeed : CheckBox = findViewById(R.id.cbox_speed)
+        val cboxTelepatia : CheckBox = findViewById(R.id.cbox_telepatia)
+
 
         /*
         *Quiero que lo que escriba en el EditText se vea en el TextView cuando haga clic sobre el
@@ -26,12 +30,25 @@ import com.google.android.material.textfield.TextInputEditText
            // val nombre = etNombre.text //Esto es lo mismo que en java nombre = etNombre.getText()
            // tvInfo.text = nombre //Esto es lo mismo que en java tvInfo.setText(nombre)
 
-            tvInfo.text = getString(R.string.info,etNombre.text.toString(),tietEstatura.text.toString().toFloat()) // Esto hace lo mismo que las 2 instrucciones anteriores
+            val genero : String = if (rbMasculino.isChecked)
+                getString(R.string.masculino)
+            else getString(R.string.femenino)
+
+            var poderes = ""
+
+            if(cboxFuerza.isChecked) poderes = getString(R.string.super_strength)
+            if(cboxSpeed.isChecked) poderes = poderes+" "+getString(R.string.speed)
+            if(cboxTelepatia.isChecked) poderes = poderes+" "+getString(R.string.telepatia)
+
+            tvInfo.text = getString(R.string.info,etNombre.text.toString(),tietEstatura.text.toString().toFloat(), genero, poderes) // Esto hace lo mismo que las 2 instrucciones anteriores
+            /**Arriba se ve el uso de un placeholder*/
 
             //val string:Float = tietEstatura.text.toString().toFloat()
 
             //Si quisiera almacenar la info en una base de datos solo sería llamar el método y ponerle
             //la data que quiero enviar cuando haga click
+
+
 
         }
     }
